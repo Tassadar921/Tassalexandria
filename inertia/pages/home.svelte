86b336d72bc65c4c6810a792ca12e2a8f090463b
Paul Cuisinier <paul.lecuisinier@gmail.com>
2024-12-29 22:29:19 +0100

    setup

---
+++ inertia/pages/home.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let user;
+
+  $: console.log(user);
+</script>
+
+<title>coucou</title>
---
+++ start/routes.ts
@@ -0,0 +1,6 @@
+import router from '@adonisjs/core/services/router'
+import { HttpContext } from '@adonisjs/core/http'
+
+router.get('/', ({ inertia }: HttpContext) => {
+  return inertia.render('home', { user: { name: 'julien' } })
+})
