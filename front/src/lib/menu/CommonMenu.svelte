195de85a3fb4108d6c0f1161af6b58f3fbba5385
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-01-02 18:38:49 +0100

    menu

--- front/src/lib/menu/CommonMenu.svelte
+++ front/src/lib/menu/CommonMenu.svelte
@@ -2,13 +2,14 @@
     import MenuItem from './MenuItem.svelte';
     import { t } from 'svelte-i18n';
     import { profile } from '../../stores/profileStore.js';
-    import FileUpload from '../shared/FileUpload.svelte';
 
     export let footer = false;
 </script>
 
 <ul class={`${footer ? '' : 'space-y-4'} p-4`}>
     <MenuItem {footer} iconLeft="home" href="/" on:click>{$t('menu.home')}</MenuItem>
+    <MenuItem {footer} iconLeft="search" href="/browse" on:click>{$t('menu.browse')}</MenuItem>
+    <MenuItem {footer} iconLeft="upload" href="/upload" on:click>{$t('menu.upload')}</MenuItem>
     {#if $profile}
         <MenuItem {footer} iconLeft="user" href="/profile" on:click>{$t('menu.profile')}</MenuItem>
         <MenuItem {footer} iconLeft="userRemove" href="/logout" on:click>{$t('menu.logout')}</MenuItem>
