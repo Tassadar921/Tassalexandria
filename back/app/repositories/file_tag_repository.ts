79ec6b4fc509ac1db436cf02b4b9716b7734b03b
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-01-06 22:29:26 +0100

    Update file_tag_repository.ts

--- back/app/repositories/file_tag_repository.ts
+++ back/app/repositories/file_tag_repository.ts
@@ -1,7 +1,6 @@
 import BaseRepository from '#repositories/base/base_repository';
 import { inject } from '@adonisjs/core';
 import FileTag from '#models/file_tag';
-import UploadedFile from '#models/uploaded_file';
 
 @inject()
 export default class FileTagRepository extends BaseRepository<typeof FileTag> {
